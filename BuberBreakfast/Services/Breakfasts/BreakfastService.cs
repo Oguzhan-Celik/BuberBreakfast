535b2a501c7263f8f4392e4a14376901541c83a5
Your Name <oguz75364@gmail.com>
2022-08-16 17:29:39 +0300

    BuberBreakfast_Api

---
+++ BuberBreakfast/Services/Breakfasts/BreakfastService.cs
@@ -0,0 +1,41 @@
+namespace BuberBreakfast.Services.Breakfast;
+using BuberBreakfast.Models;
+using ErrorOr;
+using BuberBreakfast.ServiceErrors;
+
+public class BreakfastService : IBreakfastService
+{
+    private static readonly Dictionary<Guid,Breakfast> _breakfast = new();
+
+    public ErrorOr<Created> CreateBreakfast(Breakfast breakfast)
+    {
+        _breakfast.Add(breakfast.Id, breakfast);
+
+        return Result.Created;
+    }
+
+    public ErrorOr<Deleted> DeleteBreakfast(Guid id)
+    {
+        _breakfast.Remove(id);
+
+        return Result.Deleted;
+    }
+
+    public ErrorOr<Breakfast> GetBreakfast(Guid id)
+    {
+        if(_breakfast.TryGetValue(id, out var breakfast))
+        {
+            return breakfast;
+        }
+
+        return Errors.Breakfast.NotFound;
+    }
+
+    public ErrorOr<UpsertedBreakfast> UpsertBreakfast(Breakfast breakfast)
+    {
+        var IsNewlyCreated = !_breakfast.ContainsKey(breakfast.Id);
+        _breakfast[breakfast.Id] = breakfast;
+
+        return new UpsertedBreakfast(IsNewlyCreated);
+    }
+}
